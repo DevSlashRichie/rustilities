1344e074bd774f16381081b0758bac6bd033a217
DevSlashRichie <ricardo@mymetaverse.io>
2023-05-04 20:41:00 -0600

    chore: added docs

--- packages/utilities/src/exception.ts
+++ packages/utilities/src/exception.ts
@@ -1,5 +1,13 @@
 import { Option } from "./option";
 
+/**
+ * This class is used to create exceptions.
+ * The purpose is to have a more readable and predictable format.
+ *
+ * You can extends this class from a custom Exception class
+ * which can provide more context.
+ *
+ **/
 export class Exception extends Error {
   public readonly parent: Option<Error>;
 
@@ -8,6 +16,10 @@ export class Exception extends Error {
     this.parent = parent;
   }
 
+  /**
+   * Use this method to display the exception in a human readable format.
+   * @returns a string
+   **/
   public display(): string {
     const className = this.constructor.name;
     return `${className}: ${this.message} ${this.parent
@@ -21,10 +33,24 @@ export class Exception extends Error {
       .unwrapOr("")}`.trim();
   }
 
+  /**
+   * Use this method to extend an exception with another exception.
+   * This means the current exception will have the second as parent.
+   *
+   * @param other: The new parent exception.
+   * @return a new exception cloning the current message with a new parent.
+   **/
   public extend(other: Exception): Exception {
     return new Exception(this.message, Option.of(other));
   }
 
+  /**
+   * Use this method to create a new simple exception.
+   *
+   * @param message The message of the exception
+   * @param parent In case there's a parent exception.
+   * @returns A new exception
+   **/
   public static create(message: string, parent: Option<Error>): Exception {
     return new Exception(message, parent);
   }
--- packages/utilities/src/option.ts
+++ packages/utilities/src/option.ts
@@ -1,5 +1,8 @@
 import { Result } from "./result";
 
+/**
+ * Use this class to create an Option type.
+ **/
 export class Option<T> {
   private readonly value?: T;
 
@@ -7,30 +10,57 @@ export class Option<T> {
     this.value = value;
   }
 
+  /**
+   * Use this method to create an Option type from a value.
+   **/
   public static of<U>(value: U): Option<U> {
     return new Option<U>(value);
   }
 
+  /**
+   * Use this method to create an empty Option type.
+   **/
   public static empty<U>(): Option<U> {
     return new Option<U>();
   }
 
+  /**
+   * Use this method to create an Option type from a value.
+   * @returns an Option with a value.
+   **/
   public static Some<U>(value: U): Option<U> {
     return new Option<U>(value);
   }
 
+  /**
+   * Use this method to create an empty Option type.
+   * @returns an empty Option.
+   **/
   public static None<U>(): Option<U> {
     return new Option<U>();
   }
 
+  /**
+   * Use this method to check if the Option is empty or not.
+   * @returns true if the Option is not empty.
+   **/
   public isSome(): boolean {
     return typeof this.value !== "undefined";
   }
 
+  /**
+   * Use this method to check if the Option is empty or not.
+   * @returns true if the Option is empty.
+   **/
   public isNone(): boolean {
     return typeof this.value === "undefined";
   }
 
+  /**
+   * Use this method to unwrap the value of the Option.
+   * @throws an error if the Option is empty.
+   * @returns the value of the Option.
+   **/
   public unwrap(): T {
     if (this.isNone()) {
       throw new Error("Cannot unwrap a None value");
@@ -38,6 +68,10 @@ export class Option<T> {
     return this.value as T;
   }
 
+  /**
+   * Use this method to unwrap the value of the Option or return a default value.
+   * @returns the value of the Option or the default value.
+   **/
   public unwrapOr(defaultValue: T): T {
     if (this.isNone()) {
       return defaultValue;
@@ -45,6 +79,10 @@ export class Option<T> {
     return this.value as T;
   }
 
+  /**
+   * Use this method to unwrap the value of the Option or
+   * return a default value from a function.
+   **/
   public unwrapOrElse(defaultValue: () => T): T {
     if (this.isNone()) {
       return defaultValue();
@@ -52,6 +90,10 @@ export class Option<T> {
     return this.value as T;
   }
 
+  /**
+   * Use this method to modify the value of the Option while using it.
+   * @returns a new Option with the new value.
+   **/
   public map<U>(mapper: (value: T) => U): Option<U> {
     if (this.isNone()) {
       return Option.empty<U>();
@@ -59,6 +101,12 @@ export class Option<T> {
     return Option.of(mapper(this.value as T));
   }
 
+  /**
+   * Use this method to modify the value of the Option while using it.
+   * In case the Option is empty, it will return the default value.
+   *
+   * @returns a new Option with the new value.
+   **/
   public mapOr<U>(defaultValue: U, mapper: (value: T) => U): U {
     if (this.isNone()) {
       return defaultValue;
@@ -66,6 +114,12 @@ export class Option<T> {
     return mapper(this.value as T);
   }
 
+  /**
+   * Change the option into a Result type.
+   * In case of None, it will return a defined error.
+   *
+   * @returns a Result type.
+   **/
   public okOr(error: Error): Result<T, Error> {
     if (this.isNone()) {
       return Result.Err(error);
@@ -74,6 +128,11 @@ export class Option<T> {
     return Result.Ok(this.value as T);
   }
 
+  /**
+   * If the Option is empty, it will return a desired option.
+   *
+   * @returns the default Option type.
+   **/
   public or(defaultValue: Option<T>): Option<T> {
     if (this.isNone()) {
       return defaultValue;
@@ -81,6 +140,11 @@ export class Option<T> {
     return this;
   }
 
+  /**
+   * Check if the wrapped value matches the desired value.
+   *
+   * @returns true if the value matches.
+   **/
   public contains(value: T): boolean {
     if (this.isNone()) {
       return false;
@@ -88,6 +152,10 @@ export class Option<T> {
     return this.value === value;
   }
 
+  /**
+   * Create a new Option from a possible empty value.
+   * @returns a new Option.
+   **/
   public static fromNullable<U>(value: U | null | undefined): Option<U> {
     if (value === null || value === undefined) {
       return Option.empty();
@@ -96,10 +164,17 @@ export class Option<T> {
   }
 }
 
+/**
+ * Use this method to create an Option type from a value.
+ * @returns an Option with a value.
+ **/
 export function Some<T>(value: T): Option<T> {
   return Option.Some(value);
 }
 
+/**
+ * Use this method to create an empty Option type.
+ **/
 export function None<T>(): Option<T> {
   return Option.None();
 }
